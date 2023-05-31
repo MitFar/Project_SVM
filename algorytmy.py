4f6886b2fbc670151831c6119c3050dd18cf4749
MitFar <82674227+MitFar@users.noreply.github.com>
2023-05-31 19:20:27 +0200

    ova, komenty, chyba ok jes

--- algorytmy.py
+++ algorytmy.py
@@ -45,8 +45,10 @@ class one_vs_one(BaseEstimator): #
 
         for class_a, class_b in combinations(self.classes, 2): #wybiera wszystkie możliwe pary
             szablon = (y==class_a) | (y == class_b) #True jeśli należą do jednej z dwóch klas ('a' lub 'b') jak nie to False
-            para_X = X[szablon]
-            para_y = y[szablon]
+            para_X = X[szablon] # tylko te elementy ze zbioru które są true, podzbiór danych treningowych dla danej pary klas
+            #print('X[szablon] OVO: ', para_X) #print daje wartości danych treningowych [1,3222 -0,541424 2,55555 1,2234] itd
+            para_y = y[szablon] #etykiety ktore odpowiadaja wartosci TRUE, podzbior etykiet dla danej pary klas
+            #print('y[szablon] OVO: ', para_y) #print wyswietla arraya podzbioru etykiet dla kazdej pary klas [1 2 1 2 2 2 1 1] [0 0 00 011] itd.
 
             classif = aSVM()
             classif.fit(para_X, para_y)
@@ -78,22 +80,31 @@ class one_vs_all(BaseEstimator):
 
     def fit(self, X, y):
         self.classes = np.unique(y).flatten() #unique wyznacza unikalne etykiety, flatte splaszcza tablice
-        for etykieta_klasy in self.classes:
-            binarna_etykieta = np.where(y == etykieta_klasy, 1, 0) #jeśli etykieta należy do klasy 1 jeśli należy do każdej innej to 0
-            classif = aSVM()
-            classif.fit(X, binarna_etykieta)
-            self.classifiers.append((etykieta_klasy, classif))
+        self.ilosc_klas = len(self.classes) #oblicza liczbe klas na podstawie unikalnych etykiet
+        self.classifiers = [] #pusta tablica na pary (etykieta klasy, klasyfikator), wielkosc macierzy 2 na ilosc poszczegolnych klas
+        for etykieta_klasy in self.classes: #iteracja po unikalnych etykietach klasy
+            binarna_etykieta = np.where(y == etykieta_klasy, 1, -1) #jeśli etykieta należy do klasy 1 jeśli należy do każdej innej to -1!!!!
+            classif = aSVM() #użycie aSVMa
+            classif.fit(X, binarna_etykieta) #trenowanie klasyfikatora na podstawie danych X i etykiet binarnych
+            #po wykonaniu fita klasyfikator gotowy jesy do przewidywania etykiet
+            self.classifiers.append((etykieta_klasy, classif)) #dodaje parę (etkieta, klasyfikator) do listy  classifiers, append(rekord dodawany na koncu listy)
+
     def predict(self, X_test):
-        przewidywane_etykiety = [] #przechowalnia przewidywan klas dla X_testa
 
-        for probka in X_test:
-            class_votes = [0] * len(self.classes) #wyniki głosów class_votes tablica wypelniona zerami
-
-            for etykieta_klasy, classif in self.classifiers: #iteracja po każdej parze (etkieta klasy, klsyfikator)
-                glos = classif.predict([probka])[0]  #wynik predykcji dla probka, [0] aby uzyskac pierwszy element z listy
-
-                if glos > 0: #jeśli wartosc glosu jest większa od 0 to klasyfikator przewidzial ze probka nalezy do klasy
-                    class_votes[etykieta_klasy] +=1
-            przewidywanie_klass = np.argmax(class_votes) #wybiera indeks klasy z najwieksza liczba glosow, przewidywana klasa
-            przewidywane_etykiety.append(przewidywanie_klass) #dodaje klase(etykiete) do listy przewidzianych etykiet
-        return przewidywane_etykiety
+        ilosc_probek = X_test.shape[0]  #ilosc probek testowych
+        macierz_wsparc = np.zeros((ilosc_probek, self.ilosc_klas)) #macierz o wymiarach(ilosc probek na ilosc klas) wypełniona zerami, celem jest przechowyanie wsparcia dla klas
+        for i in range(self.ilosc_klas): #iteracja po klasach
+            etykieta_klasy, clf = self.classifiers[i] #przypisanie wartosci z listy classifiers[i](w ktorej są pary etkieta klasy, klasyfikator) do zmiennych etykieta_klasy i clf
+            wsparcie = clf.predict(X_test) #zwraca wartość wsparcia dla danych testowych
+            #print('Wsparcie predict OVA: ', wsparcie) #printuje liste 1 i -1
+            macierz_wsparc[:,i] = wsparcie #przypisanie wartosci wsparcia dla danej klasy do odpowiedniej kolumny macierzy wsparcia
+            """
+            macierz_wsparcia przechowuje wartosci wsparcia klas dla wszystkich probek testowych, iteracja przez wszystkie klasy;
+            dla kazdej klasy 'wsparcie' bedzie zawierac wartosci wsparcia dla probek testowych dla konkretnej klasy
+            """
+            #print('Macierz wsparć predict OVA: ', macierz_wsparc[:,i]) #printuje liste 1 i -1
+        przewidywane_etykiety  = np.argmax(macierz_wsparc, axis=1)#argmax zwraca index elementu o najwiekszej wartosci a tym wypadku etykiete
+        # o najwiekszym wsparciu, axis=1 - przeszukanie w każdym wierszu macierzy,
+        # przywidywanie_etykiet to tablica z indekasami kolumn z najwiekszymi wsparciami dla kazdego wiersza macierzy macierz_wsparc
+        #print('PRZEWIDYWANIE ETYKIET PREDICT OVA: ', przewidywane_etykiety) #print daje liste etykiet[0 1 2.....]
+        return przewidywane_etykiety #zwraca przewidywanie_etykiet
